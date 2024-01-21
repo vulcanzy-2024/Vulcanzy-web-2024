62b1e96f2053d59a4570c171f21fdf25236e90bf
asmdnayeeem <asmdnayeem245@gmail.com>
2024-01-21 12:53:42 +0530

    naming changes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-
-     import HostelAccomodation from './pages/HostelAccomodation';
-import { Route, Routes } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import Home from './pages/Home';
-import './App.css';
-import EventsPage from './pages/Events';
-import Gallery from './pages/Gallery';
+import HostelAccomodation from "./pages/HostelAccomodation";
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import "./App.css";
+import EventsPage from "./pages/Events";
+import Gallery from "./pages/Gallery";
 
 const App = () => {
   return (
-    <div className='min-h-[100vh] flex flex-col  overflow-x-hidden'>
+    <div className="min-h-[100vh] flex flex-col  overflow-x-hidden">
       <Navbar />
       <Routes>
-        <Route path='/hostelAccomodation' element={<HostelAccomodation />} />
-        <Route path='/' element={<Home/>}/>
-        <Route path='/events' element={<EventsPage/>}/>
-        <Route path='/gallery' element={<Gallery/>} />
+        <Route path="/hostelAccomodation" element={<HostelAccomodation />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/events" element={<EventsPage />} />
+        <Route path="/gallery" element={<Gallery />} />
       </Routes>
       <Footer />
     </div>
   );
-}
+};
 
 export default App;
