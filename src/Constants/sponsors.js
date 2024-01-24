d26eef3ac74b5527aad5f1915a12ca00addfe1d7
asmdnayeeem <asmdnayeem245@gmail.com>
2024-01-24 13:46:19 +0530

    sponsors change

--- src/Constants/sponsors.js
+++ src/Constants/sponsors.js
@@ -4,8 +4,7 @@ import c from "../assets/images/sponsors/coca-cola-logo-01.jpg";
 import d from "../assets/images/sponsors/selection_101-120x120.png";
 import e from "../assets/images/sponsors/logo_2ef487fcede9d3552a96f2cfa12096ec_1x.png";
 import f from "../assets/images/sponsors/unstop-logo.svg";
-import g from "../assets/images/sponsors/idzratgZ5F.png";
-// import g from "../assets/images/sponsors/sbi.svg";
+
 
 const sponsors = [
   {
@@ -32,10 +31,6 @@ const sponsors = [
     images: f,
     title: "",
   },
-  {
-    images: g,
-    title: "",
-  }
 ];
 
 export default sponsors;
