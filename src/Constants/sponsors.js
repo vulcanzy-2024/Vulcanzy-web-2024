edb13e77d273fde8d7e70713c80de424600041cf
asmdnayeeem <asmdnayeem245@gmail.com>
2024-01-27 12:37:39 +0530

    sponsors changes

--- src/Constants/sponsors.js
+++ src/Constants/sponsors.js
@@ -4,10 +4,15 @@ import c from "../assets/images/sponsors/coca-cola-logo-01.jpg";
 import d from "../assets/images/sponsors/selection_101-120x120.png";
 import e from "../assets/images/sponsors/logo_2ef487fcede9d3552a96f2cfa12096ec_1x.png";
 import f from "../assets/images/sponsors/unstop-logo.svg";
-
+import lic from "../assets/images/sponsors/LIc.png"
 
 const sponsors = [
   {
+    imp:3,
+    images:lic
+  },
+  {
+    imp:1,
     images: a,
     title: "Title Sponsor",
   },
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -112,53 +112,37 @@ const Home = () => {
           />
         </div>
       </div>
-      {/* 
-      <div className="bg-black w-full  flex justify-center font-lazy items-center overflow-clip relative -z-20">
-        <div className="bg-right_wave  bg-contain w-[20rem]  bg-no-repeat h-[20rem] sm:h-[25rem] md:h-[30rem] sm:w-[25rem] md:w-[30rem]  absolute -right-[10rem] sm:-right-[11rem] md:-right-[15rem]   -z-10"></div>
-        <div className="bg-left_wave  w-[20rem] bg-contain   bg-no-repeat h-[20rem] sm:h-[25rem] md:h-[30rem] sm:w-[25rem] md:w-[30rem]  absolute -left-[10rem] sm:-left-[12rem] md:-left-[16rem]  "></div>
-        <div className="grid relative -top-20  grid-cols-2 gap-12 grid-rows-2 justify-items-center   w-[70%] h-[50%]">
-          <div className="  p-5 self-center relative w-[15rem] h-[15rem] sm:w-[25rem]  sm:h-[25rem] md:w-[35rem] md:h-[35rem] bg-contain   bg-no-repeat flex justify-center items-center">
-            <img src={bubble} className="absolute " alt="" />
-            <p className=" text-[1.2rem] sm:text-[1.8rem] md:text-[2.2rem] z-[2] ">Events</p>
-          </div>
-          <div className="bg-blue_drop  self-center w-[15rem] h-[15rem] sm:w-[25rem]  sm:h-[25rem] md:w-[35rem] md:h-[35rem]  bg-contain   bg-no-repeat flex justify-center items-center">
-            <p className=" text-[1.2rem] sm:text-[1.8rem] md:text-[2.2rem]  text-center  ">Gallery</p>
-          </div>
-          <div className="  col-span-2 self-center  flex justify-center">
-            <div className="  bg-purple_drop  w-[15rem] h-[15rem]  sm:w-[30rem]  sm:h-[30rem] md:w-[35rem] md:h-[35rem]  bg-contain   bg-no-repeat flex justify-center items-center -mt-60 sm:mt-0">
-              <p className=" text-[1.2rem] sm:text-[1.8rem] md:text-[2.2rem] text-center text-wrap w-[9rem] -ml-5 sm:-ml-[3rem] sm:mt-9 md:mt-5">
-                Accomodation Details
-              </p>
-            </div>
-          </div>
-        </div>
-      </div> */}
 
-      <div className="w-full  bg-black flex justify-center flex-col items-center relative overflow-clip -z-30">
-        <p className="text-white font-cdr text-2xl sm:text-3xl md:text-4xl">
-          Our Sponsors
-        </p>
-        {/* <div className="opacity-60 bg-blue_bubbles w-[20rem] h-[20rem] absolute -right-[4rem] bottom-0 sm:w-[30rem] sm:h-[30rem] bg-contain   bg-no-repeat -z-10"></div>
+      <div className="bg-black -z-50 sca">
+        <div className="w-full max-h-max  bg-black flex justify-center flex-col items-center relative overflow-clip -z-30">
+          <p className="text-white font-cdr  text-3xl">Our Sponsors</p>
+          {/* <div className="opacity-60 bg-blue_bubbles w-[20rem] h-[20rem] absolute -right-[4rem] bottom-0 sm:w-[30rem] sm:h-[30rem] bg-contain   bg-no-repeat -z-10"></div>
         <div className="opacity-60 bg-pastle w-[20rem] h-[20rem] sm:w-[30rem] sm:h-[30rem] absolute -left-[8rem] top-5 sm:-left-[12rem] sm:-top-10 bg-contain   bg-no-repeat -z-20"></div> */}
-        <div className="h-[90%] flex flex-wrap justify-center items-center md:grid grid-cols-3 justify-items-center w-full">
-          {sponsors.map((sponsor, index) => (
-            <div
-              className="flex justify-center flex-col items-center m-5 gap-5"
-              key={index}
-            >
-              <img
-                src={sponsor.images}
-                className="w-auto h-[4rem] sm:h-[5rem] md:h-[6rem]"
-                alt=""
-              />
-              <p className="text-white font-cdr text-center sm:text-xl md:text-2xl">
-                {sponsor.title}{" "}
-              </p>
-            </div>
-          ))}
+          <div className="h-[90%] flex flex-wrap justify-center items-center md:grid grid-cols-3 justify-items-center w-full">
+            {sponsors.map((sponsor) => (
+              <div className="flex justify-center flex-col items-center m-5 gap-5">
+                {sponsor.imp ? (
+                  <img
+                    src={sponsor.images}
+                    className="w-auto h-[7rem] sm:h-[8rem] md:h-[9rem]"
+                    alt=""
+                  />
+                ) : (
+                  <img
+                    src={sponsor.images}
+                    className="w-auto h-[4rem] sm:h-[5rem] md:h-[5rem]"
+                    alt=""
+                  />
+                )}
+                {/* <img src={sponsor.images} className="w-auto h-[6rem]" alt="" /> */}
+                <p className="text-white font-cdr text-center text-2xl">
+                  {sponsor.title}{" "}
+                </p>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
-
       <div className="bg-black flex justify-center relative h-[10rem] w-full items-center ">
         <a
           href="https://forms.gle/ioxrYNX1bx1nrogp7"
